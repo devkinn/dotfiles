d05bb87e3933e81d47497a53fa755e75fd717b0d
devkinn <jakub.kaczmarek.888@gmail.com>
2025-02-21 21:14:54 +0100

    feat: add dotfiles

---
+++ nvim/.config/nvim/lua/plugins/colorscheme.lua
@@ -0,0 +1,12 @@
+return {
+  { "f4z3r/gruvbox-material.nvim", name = "gruvbox-material" },
+  { "EdenEast/nightfox.nvim" },
+
+  -- Configure LazyVim to load gruvbox
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "carbonfox",
+    },
+  },
+}
