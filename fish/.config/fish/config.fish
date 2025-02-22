4f5af211d0ce92a6f530707701a57a019397eb7e
devkinn <jakub.kaczmarek.888@gmail.com>
2025-02-22 21:37:30 +0100

    fish: add dotfiles

---
+++ fish/.config/fish/config.fish
@@ -0,0 +1,12 @@
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+end
+
+set -g fish_greeting
+
+alias ll="eza -l --icons"
+alias lla="eza -al --icons"
+
+set -gx SSH_AUTH_SOCK "$XDG_RUNTIME_DIR/ssh-agent.socket"
+
+starship init fish | source
