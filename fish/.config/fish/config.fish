60d63e83615f92e20c4e8f66c7a9a198f7344a98
devkinn <jakub.kaczmarek.888@gmail.com>
2025-02-23 13:32:35 +0100

    fish: add alias for neovim

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -7,6 +7,8 @@ set -g fish_greeting
 alias ll="eza -l --icons"
 alias lla="eza -al --icons"
 
+alias v="nvim"
+
 set -gx SSH_AUTH_SOCK "$XDG_RUNTIME_DIR/ssh-agent.socket"
 
 starship init fish | source
