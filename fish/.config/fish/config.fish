16840fe7f44dbfe0fa669ffc9a111118741aaecd
devkinn <jakub.kaczmarek.888@gmail.com>
2025-03-04 15:02:09 +0100

    fish: add comment to config.fish file

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -9,7 +9,7 @@ alias lla="eza -al --icons"
 
 alias v="nvim"
 
-set -gx SSH_AUTH_SOCK "$XDG_RUNTIME_DIR/ssh-agent.socket"
+set -gx SSH_AUTH_SOCK "$XDG_RUNTIME_DIR/ssh-agent.socket" # Add environment variable for ssh-agent systemd automatic startup
 
 set -U fish_user_paths $HOME/.local/bin $fish_user_paths
 
